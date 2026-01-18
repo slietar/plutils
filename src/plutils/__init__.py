74ea37d7ae9e88f50ea9740608379205736e2ab1
Simon Liétar <simon@lietar.net>
2026-01-18 04:23:09 +0100

    Add struct and zip functions

--- src/plutils/__init__.py
+++ src/plutils/__init__.py
@@ -1,8 +1,9 @@
+from collections.abc import Iterable
 from pathlib import Path
 from typing import TypeAlias, overload
 
 import polars as pl
-from polars._typing import IntoExpr, PythonLiteral
+from polars._typing import IntoExpr, NonNestedLiteral
 from polars.plugins import register_plugin_function
 
 from . import extension # type: ignore
@@ -10,7 +11,7 @@ from . import extension # type: ignore
 
 PLUGIN_PATH = Path(__file__).parent
 
-ExprLike: TypeAlias = PythonLiteral | pl.Expr | str
+ExprLike: TypeAlias = NonNestedLiteral | pl.Expr | str
 
 
 @overload
@@ -109,9 +110,65 @@ def implode_with_offsets(target: ExprLike | pl.Series, /, offsets: IntoExpr | pl
     )
 
 
+@overload
+def struct(*fields: pl.Series) -> pl.Series:
+    ...
+
+@overload
+def struct(fields: Iterable[pl.Series], /) -> pl.Series:
+    ...
+
+def struct(*fields: pl.Series | Iterable[pl.Series]):
+    if not fields or isinstance(fields[0], pl.Series):
+        effective_fields: tuple[pl.Series, ...] = tuple(fields) # type: ignore
+    else:
+        effective_fields = tuple(fields[0])
+
+    if len(effective_fields) == 0:
+        raise ValueError("At least one series must be provided")
+
+    known_names = set[str]()
+
+    for field in effective_fields:
+        if field.name in known_names:
+            raise ValueError(f"Duplicate field name in struct: {field.name!r}")
+
+        known_names.add(field.name)
+
+    return pl.DataFrame(effective_fields).to_struct()
+
+
+@overload
+def zip(*targets: pl.Series) -> pl.Series:
+    ...
+
+@overload
+def zip(targets: Iterable[pl.Series], /) -> pl.Series:
+    ...
+
+def zip(*targets: pl.Series | Iterable[pl.Series]):
+    if not targets or isinstance(targets[0], pl.Series):
+        effective_targets: tuple[pl.Series, ...] = tuple(targets) # type: ignore
+    else:
+        effective_targets = tuple(targets[0])
+
+    if len(effective_targets) == 0:
+        raise ValueError("At least one series must be provided")
+
+
+    return implode_like(
+        struct(
+            target.list.explode() for target in effective_targets
+        ),
+        layout=effective_targets[0],
+    )
+
+
 __all__ = [
     "get_offsets",
     "implode_like",
     "implode_with_lengths",
     "implode_with_offsets",
+    "struct",
+    "zip",
 ]
